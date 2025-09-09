ac12cdcdbca7cca9ef276d2e841fa514e7bfbc2d
alidiamond1 <calilucky3@gmail.com>
2025-09-09 20:05:55 +0300

    frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 export const api = axios.create({
